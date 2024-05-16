6ee643e005e7b4ce0d69996afc936d73088e13a5
sreecharan7 <115004416+sreecharan7@users.noreply.github.com>
2024-05-17 00:32:45 +0530

    install.sh:updated commands

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cp -r ./src /usr/local/src/run
+rm -rf /usr/local/src/run/
+mv -f ./src /usr/local/src/run
 
-cp ./run /usr/bin
+mv -f ./run /usr/bin
 
 chmod +x /usr/bin/run
 
-echo "installed sucessfully\n deleting source files"
+echo -e "installed sucessfully\n deleting source files"
 
-rm -fr ./src
-rm  -f run
-rm -f install.sh
+cd .. 
+rm -rf runner
