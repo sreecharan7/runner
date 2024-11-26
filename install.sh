bae9ef3700898450cbf07094246c841e3ef219f6
sree charan <sreecharanreddypacharla@gmail.com>
2024-11-26 23:38:41 +0530

    added alias feature for wsl and windows integration

--- install.sh
+++ install.sh
@@ -9,6 +9,15 @@ dir_dist="${dir_src}/local/src/run"
 if ! grep -q "microsoft" /proc/version; then
     echo "removing some unnecessary files..."
     rm -rf ./src/wsl
+else
+    read -p "do you want the run comamnd avaliable in windows also [y/n]: " option
+    if [[ "${option}" == "y" ]];then
+        dest="C:\Program Files\runner\bin"
+        batfile="$(wslpath -w ./src/wsl/run.bat)"
+        echo "This may ask for the permission for running as administration mode (press yes if asked)"
+        sleep 2
+        /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -Command "& {Start-Process powershell -ArgumentList '-Command \"New-Item -ItemType Directory -Path \\\"${dest}\\\";copy \\\"${batfile}\\\" \\\"${dest}\\\";\"' -Verb RunAs}"
+    fi
 fi
 
 rm -rf "${dir_dist}"
